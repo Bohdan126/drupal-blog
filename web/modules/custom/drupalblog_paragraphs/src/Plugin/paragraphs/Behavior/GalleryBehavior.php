f5116c782dda3a7fdfbc6d49771a5464400ef4d0
bohdan126 <bohdan126@gmail.com>
2018-12-18 19:54:24 +0200

    ImageAndTextBehavior

--- web/modules/custom/drupalblog_paragraphs/src/Plugin/paragraphs/Behavior/GalleryBehavior.php
+++ web/modules/custom/drupalblog_paragraphs/src/Plugin/paragraphs/Behavior/GalleryBehavior.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\drupalblog_paragraphs\Plugin\paragraphs\Behavior;
 
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Annotation\Translation;
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Form\FormStateInterface;
@@ -21,33 +22,38 @@ use Drupal\paragraphs\ParagraphsBehaviorBase;
  */
 class GalleryBehavior extends ParagraphsBehaviorBase {
 
-    /**
-     * {@inheritdoc}
-     */
+  /**
+   * {@inheritdoc}
+   */
 
-    public static function isApplicable(ParagraphsType $paragraphs_type) {
-        return $paragraphs_type->id() == 'gallery';
-    }
-    /**
-     * Extends the paragraph render array with behavior.
-     */
-    public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
-        // TODO: Implement view() method.
-    }
+  public static function isApplicable(ParagraphsType $paragraphs_type) {
+    return $paragraphs_type->id() == 'gallery';
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
-        $form['items_per_row'] = [
-            '#type' => 'select',
-            '#title' => $this->t('Number of images per row'),
-            '#options' => [
-                '2' => $this->formatPlural(2,'1 photo per row', '@count photos per row'),
-                '3' => $this->formatPlural(3,'1 photo per row', '@count photos per row'),
-                '4' => $this->formatPlural(4,'1 photo per row', '@count photos per row'),
-            ],
-        ];
-        return $form;
-    }
+  /**
+   * Extends the paragraph render array with behavior.
+   */
+  public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
+    $images_per_row = $paragraph->getBehaviorSetting($this->getPluginId(), 'items_per_row', 4);
+    $bem_block = 'paragraph-' . $paragraph->bundle() . ($view_mode == 'default' ? '' : '-' . $view_mode);
+    $build['#attributes']['class'][] = Html::getClass($bem_block . '--images-per-row-' . $images_per_row);
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
+    $form['items_per_row'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Number of images per row'),
+      '#options' => [
+        '2' => $this->formatPlural(2, '1 photo per row', '@count photos per row'),
+        '3' => $this->formatPlural(3, '1 photo per row', '@count photos per row'),
+        '4' => $this->formatPlural(4, '1 photo per row', '@count photos per row'),
+      ],
+      '#default_value' => $paragraph->getBehaviorSetting($this->getPluginId(), 'items_per_row', 4),
+    ];
+    return $form;
+  }
 }
